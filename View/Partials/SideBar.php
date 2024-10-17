aa257d940f35e0b77a4dbe6c87c0916aa16dfb1a
Zarpec <axelza25@gmail.com>
2024-10-16 20:07:44 -0600

    se agregaron condicionales para mostrar opciones si se ha iniciado sesion

--- View/Partials/SideBar.php
+++ View/Partials/SideBar.php
@@ -8,10 +8,26 @@
     <div id="sidebar">
         <div class="elemenBar">
             <ul>
-                <li class="active"><a href="?action=login"><i class="fa-solid fa-star"></i> Cuenta</a></li>
-                <li><a href="?action=homeUser"><i class="fa-solid fa-star"></i> Pedidos</a></li>
-                <li><a href="#"><i class="fa-solid fa-star"></i> Carrito</a></li>
-                <li><a href="#"><i class="fa-solid fa-star"></i> Cerrar Sesión</a></li>
+            <li class=''><a href='?action=home'><i class='fa-solid fa-star'></i>Inicio</a></li>
+                <?php
+                if (!empty($_SESSION['user'])) {
+                    echo "
+                <li><a href='?action=homeUser'><i class='fa-solid fa-star'></i> Pedidos</a></li>
+                ";
+                }else{
+                    echo "
+                    <li class=''><a href='?action=login'><i class='fa-solid fa-star'></i> iniciar Sesión</a></li>
+                    ";
+                }
+                ?>
+                <li><a href='#'><i class='fa-solid fa-star'></i> Carrito</a></li>
+                <?php
+                if (!empty($_SESSION['user'])) {
+                    echo "
+                <li><a href='?action=logout'><i class='fa-solid fa-star'></i> Cerrar Sesión</a></li>";
+                }
+                ?>
+
             </ul>
         </div>
     </div>
